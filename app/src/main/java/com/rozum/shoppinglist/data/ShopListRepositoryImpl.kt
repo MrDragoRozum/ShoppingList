39179bb2c80a3e5adfd8b4a7d8ad472809585b60
Alexander Rozum <roozumaleks@gmail.com>
2023-06-23 17:06:21 +0300

    Реализован scroll через LinearLayout

--- app/src/main/java/com/rozum/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/rozum/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -7,8 +7,15 @@ import com.rozum.shoppinglist.domain.ShopListRepository
 object ShopListRepositoryImpl : ShopListRepository {
 
     private val listLiveData = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({o1, o2 -> o1.id.compareTo(o2.id)})
     private var autoIncrementId = 0
+
+    init {
+        for(i in 0 until 10) {
+            val shopItem = ShopItem("item $i", i, true)
+            addShopItem(shopItem)
+        }
+    }
 
     override fun addShopItem(shopItem: ShopItem) {
         if (shopItem.id == ShopItem.UNDEFINED_ID) {
--- app/src/main/java/com/rozum/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/rozum/shoppinglist/presentation/MainActivity.kt
@@ -2,15 +2,43 @@ package com.rozum.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.rozum.shoppinglist.R
+import com.rozum.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var linerLayoutItemsShop: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        linerLayoutItemsShop = findViewById(R.id.linerLayoutItemsShop)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.getShopList.observe(this) { showList(it) }
+    }
+
+    private fun showList(list: List<ShopItem>) {
+        linerLayoutItemsShop.removeAllViews()
+        for(item in list) {
+            val layout = if(item.enabled) {
+                R.layout.item_shop_enabled
+            } else {
+                R.layout.item_shop_disabled
+            }
+            val view = LayoutInflater.from(this).inflate(layout, linerLayoutItemsShop, false)
+            val textViewName = view.findViewById<TextView>(R.id.textViewName)
+            val textViewCount = view.findViewById<TextView>(R.id.textViewCount)
+            textViewName.text = item.name
+            textViewCount.text = item.score.toString()
+            view.setOnLongClickListener {
+                viewModel.changeEnabledStatus(item)
+                true
+            }
+            linerLayoutItemsShop.addView(view)
+        }
     }
 }
